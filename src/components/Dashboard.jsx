bb0f121117c59d81fe92db9d9c470130da680c5c
Vinayak Rastogi <vinayakr841@gmail.com>
2026-07-27 14:59:07 +0530

    Auth fix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import api from "../api/axios";
 
 import Apps from "./Apps";
 import Funds from "./Funds";
 import Holdings from "./Holdings";
-
 import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
@@ -12,14 +12,52 @@ import WatchList from "./WatchList";
 import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        await api.get(
+          "/verify",
+          {
+            withCredentials: true,
+          }
+        );
+
+        setLoading(false);
+      } catch (err) {
+        window.location.href =
+          "https://finora-y3fy.netlify.app/login";
+      }
+    };
+
+    verifyUser();
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="dashboard-container">
       <GeneralContextProvider>
         <WatchList />
       </GeneralContextProvider>
+
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Summary />} />
+          <Route path="/" element={<Summary />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
